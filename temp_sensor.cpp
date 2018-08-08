26f087706706962f66b16aeac18749d625c7fc87
lupyuen <luppy@appkaki.com>
2018-08-08 13:19:47 +0800

    Refactoring sensor framework

--- temp_sensor.cpp
+++ temp_sensor.cpp
@@ -10,6 +10,7 @@
 #include "sensor.h"
 #include "temp_sensor.h"
 
+//  These are the sensor functions that we will implement in this file.
 static uint8_t poll_sensor(void);
 static uint8_t receive_sensor_data(float *data, uint8_t size);
 static void init_sensor(uint8_t id, Evt_t *event, uint16_t period_ms);
@@ -28,10 +29,7 @@ static SensorControl sensor_control = {
   &prev_channel
 };
 
-static Sensor sensor = {
-  sensor_info,
-  sensor_control
-};
+static Sensor sensor = { sensor_info, sensor_control };
 
 static float sensorData = NAN;  //  Default the sensor data to "Not A Number".
 static uint8_t newData = 0;  //  Set to non-zero if there is new sensor data to be received.
@@ -45,7 +43,7 @@ static void init_sensor(uint8_t id, Evt_t *event, uint16_t poll_interval) {
 static uint8_t poll_sensor(void) {
   //  Poll the sensor for new data. Return size of new data if new data available, 0 otherwise.
   debug("temp.poll_sensor"); ////
-
+  
   //  Read sensor data from BME280.
   extern BME280I2C bme;  ////  TODO
   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
@@ -54,7 +52,7 @@ static uint8_t poll_sensor(void) {
   //  Simulated sensor.
   //// sensorData = 12.3 + rand() % 10;
 
-  newData = 1;  //  New sensor data now available.
+  newData = 1;  //  New sensor data now available, size is 1 float.
   return newData;  //  Data should always be available.
 }
 
@@ -67,12 +65,16 @@ static uint8_t receive_sensor_data(float *data, uint8_t size) {
   return 1;  //  Only 1 float returned.
 }
 
+Sensor *get_temp_sensor(void) {
+  //  Return the global instance of the sensor.
+  return &sensor;
+}
+
 static void next_channel(void) {
+  //  Not used.
 }
 
 static void prev_channel(void) {
+  //  Not used.
 }
 
-Sensor *get_temp_sensor(void) {
-  return &sensor;
-}
