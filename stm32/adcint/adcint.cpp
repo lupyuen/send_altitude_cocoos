1a5d35ce96baeff06151e1585b444954d2c6276a
lupyuen <luppy@appkaki.com>
2018-11-25 13:54:39 +0800

    Adding ADC

--- stm32/adcint/adcint.cpp
+++ stm32/adcint/adcint.cpp
@@ -15,31 +15,33 @@
 //  TODO: Update this when running under FreeRTOS.
 static void taskYIELD(void) {}
 
-static uint16_t read_adc(uint8_t channel) {
- 	//  Read ADC Channel
-	adc_set_sample_time(ADC1,channel,ADC_SMPR_SMP_239DOT5CYC);
-	adc_set_regular_sequence(ADC1,1,&channel);
+uint16_t adc_read(uint8_t channel) {
+	//  Read ADC1. Channel=0 for PA0, 1 for PA1.
+	adc_set_sample_time(ADC1, channel, ADC_SMPR_SMP_239DOT5CYC);
+	adc_set_regular_sequence(ADC1, 1, &channel);
 	adc_start_conversion_direct(ADC1);
 	while ( !adc_eoc(ADC1) )
 		taskYIELD();
 	return adc_read_regular(ADC1);
 }
 
-static int degrees_C100(void) {
- 	//  Return temperature in C * 100
+int adc_read_scaled_temperature(void) {
+	//  Return Blue Pill temperature in degrees C scaled by 100 times, e.g. 36.9 C is returned as 3690.
 	static const int v25 = 143;
-	int vtemp = (int)read_adc(ADC_CHANNEL_TEMP) * 3300 / 4095;
+	int vtemp = (int)adc_read(ADC_CHANNEL_TEMP) * 3300 / 4095;
 	return (v25 - vtemp) / 45 + 2500; // temp = (1.43 - Vtemp) / 4.5 + 25.00
 }
 
 static void demo_task(void *arg __attribute((unused))) {
-	// int vref = read_adc(ADC_CHANNEL_VREF) * 330 / 4095;
-	int adc0 = read_adc(0) * 330 / 4095;
-	int adc1 = read_adc(1) * 330 / 4095;
-	int temp = degrees_C100();
+	// int vref = adc_read(ADC_CHANNEL_VREF) * 330 / 4095;
+	int adc0 = adc_read(0) * 330 / 4095;
+	int adc1 = adc_read(1) * 330 / 4095;
+	int temp = adc_read_scaled_temperature();
 }
 
 void adc_setup(void) {
+	//  Configures ADC1 as ADC Input, PA0 and PA1 as Analog In pins.
+
 	//  Moved to platform_setup() in bluepill.cpp:
 	//  rcc_clock_setup_in_hse_8mhz_out_72mhz();
 	rcc_periph_clock_enable(RCC_GPIOA);		// Enable GPIOA for ADC
@@ -47,22 +49,21 @@ void adc_setup(void) {
 		GPIO_MODE_INPUT,
 		GPIO_CNF_INPUT_ANALOG,
 		GPIO0|GPIO1);				// PA0 & PA1
-	// Initialize ADC:
-	rcc_peripheral_enable_clock(&RCC_APB2ENR,RCC_APB2ENR_ADC1EN);
+	// Initialize ADC.
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_ADC1EN);
 	adc_power_off(ADC1);
-	rcc_peripheral_reset(&RCC_APB2RSTR,RCC_APB2RSTR_ADC1RST);
-	rcc_peripheral_clear_reset(&RCC_APB2RSTR,RCC_APB2RSTR_ADC1RST);
+	rcc_peripheral_reset(&RCC_APB2RSTR, RCC_APB2RSTR_ADC1RST);
+	rcc_peripheral_clear_reset(&RCC_APB2RSTR, RCC_APB2RSTR_ADC1RST);
 	rcc_set_adcpre(RCC_CFGR_ADCPRE_PCLK2_DIV6);	// Set. 12MHz, Max. 14MHz
 	adc_set_dual_mode(ADC_CR1_DUALMOD_IND);		// Independent mode
 	adc_disable_scan_mode(ADC1);
 	adc_set_right_aligned(ADC1);
 	adc_set_single_conversion_mode(ADC1);
-	adc_set_sample_time(ADC1,ADC_CHANNEL_TEMP,ADC_SMPR_SMP_239DOT5CYC);
+	adc_set_sample_time(ADC1, ADC_CHANNEL_TEMP, ADC_SMPR_SMP_239DOT5CYC);
 	// adc_set_sample_time(ADC1,ADC_CHANNEL_VREF,ADC_SMPR_SMP_239DOT5CYC);
 	adc_enable_temperature_sensor();
 	adc_power_on(ADC1);
 	adc_reset_calibration(ADC1);
 	adc_calibrate_async(ADC1);
-	while ( adc_is_calibrating(ADC1) );
-	return 0;
+	while (adc_is_calibrating(ADC1)) {}
 }
--- stm32/adcint/adcint.h
+++ stm32/adcint/adcint.h
@@ -1,6 +1,22 @@
 //  ADC Interface for STM32
 #ifndef ADCINT_H_
 #define ADCINT_H_
+#include <stdint.h>  //  For uint16_t
 
+#ifdef __cplusplus
+extern "C" {  //  Allows functions below to be called by C and C++ code.
+#endif
 
+//  Configures ADC1 as ADC Input, PA0 and PA1 as Analog In pins.
+void adc_setup(void);
+
+//  Read ADC1. Channel=0 for PA0, 1 for PA1.
+uint16_t adc_read(uint8_t channel);
+
+//  Return Blue Pill temperature in degrees C scaled by 100 times, e.g. 36.9 C is returned as 3690.
+int adc_read_scaled_temperature(void);
+
+#ifdef __cplusplus
+}  //  End of extern C scope.
+#endif
 #endif  //  ADCINT_H_
